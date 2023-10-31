92ad09dce95fea7e554c538ab6d1c3020366af03
Alifur Rahman <alifurrahman1999@gmail.com>
2023-11-01 03:25:48 +0600

    update dashboard menu responsive

--- src/pages/userPanel/EditUserProfile.jsx
+++ src/pages/userPanel/EditUserProfile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 import DashboardMenu from '../../components/userPanel/DashboardMenu';
 import ScrollToTop from '../../utils/RouteChange';
@@ -8,26 +9,35 @@ const EditUserProfile = () => {
     ScrollToTop();
     const navigate = useNavigate();
     const [getAuthF, setAuthF] = useState(checkAuth());
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         if (!getAuthF) {
             navigate('/login');
         }
-    },[getAuthF]);
+    }, [getAuthF]);
+    const [isActiveMenu, setIsActiveMenu] = useState(false);
+
+    const handelDashMenu = () => {
+        setIsActiveMenu(!isActiveMenu);
+    }
+
 
     return (
         <section className="full_widht_auth_section">
             <div className="container">
                 <div className="dashboard">
                     {/* <!-- Dashboard Menu --> */}
-                    <DashboardMenu />
+                    <DashboardMenu isActiveMenu={isActiveMenu} />
                     {/* <!-- Add Project --> */}
                     <div className="dashboard_add_project">
                         {/* <!-- Add Project head --> */}
                         <div className="add_project_head">
+                            <button className='dasMenuBtn' onClick={handelDashMenu}>
+                                <AiOutlineMenuUnfold />
+                            </button>
                             <h3 className="title">Update Profile</h3>
                         </div>
-                       
+
                     </div>
                 </div>
             </div>
--- src/pages/userPanel/PasswordChange.jsx
+++ src/pages/userPanel/PasswordChange.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 import DashboardMenu from '../../components/userPanel/DashboardMenu';
 import ScrollToTop from '../../utils/RouteChange';
@@ -10,20 +11,29 @@ const PasswordChange = () => {
     const navigate = useNavigate();
     const [getAuthF, setAuthF] = useState(checkAuth());
     console.log(setAuthF);
-    useEffect(()=>{
+    useEffect(() => {
         if (!getAuthF) {
             navigate('/login');
         }
-    },[getAuthF]);
+    }, [getAuthF]);
+    const [isActiveMenu, setIsActiveMenu] = useState(false);
+
+    const handelDashMenu = () => {
+        setIsActiveMenu(!isActiveMenu);
+    }
+
 
     return (
         <section className="full_widht_auth_section">
             <div className="container">
                 <div className="dashboard">
-                    <DashboardMenu />
+                    <DashboardMenu isActiveMenu={isActiveMenu} />
                     <div className="dashboard_add_project">
                         {/* <!-- Add Project head --> */}
                         <div className="add_project_head">
+                            <button className='dasMenuBtn' onClick={handelDashMenu}>
+                                <AiOutlineMenuUnfold />
+                            </button>
                             <h3 className="title">Change Password</h3>
                         </div>
                     </div>
