56c4de3be0d435aca3fd0b1820075a772f35099b
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-11-14 21:06:13 +0600

    Add some css

--- src/components/LikeShare/ShowLikes.jsx
+++ src/components/LikeShare/ShowLikes.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { instance as axoisInstance } from '../../helpers/axios/axoisInstance';
 
-const ShowLikes = ({ likes, likesUsers }) => {
-  let lastThreeLikedUsers = likesUsers.slice(-3);
-
-  if (likesUsers.length <= 3) {
-    lastThreeLikedUsers = likesUsers;
+const fetchLikes = async (postId) => {
+  try {
+    const response = await axoisInstance.get(`/api/likes/${postId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching likes:', error);
+    return [];
   }
+};
+
+const ShowLikes = ({ postId }) => {
+  const [likes, setLikes] = useState(0);
+  const [likedUsers, setLikedUsers] = useState([]);
+
+  useEffect(() => {
+    fetchLikes(postId).then((data) => {
+      setLikes(data.likes.length);
+      setLikedUsers(data.users);
+    });
+  }, [postId]);
 
   return (
     <div className="project_reso_details">
-      <div className="likded_users">
-        {
-          lastThreeLikedUsers.map((user, index) => (
-            <Link to="/" key={index}>
-              <img src={user.profileImageUrl} alt={`userImage`} />
-            </Link>
-          ))
-        }
+      <div className="liked_users">
+        {likedUsers.slice(-3).map((user, index) => (
+          <Link to="/" key={index}>
+            <img src={user.profileImageUrl} alt={`userImage`} />
+          </Link>
+        ))}
       </div>
-      <p>and {likes} {likes === 1 ? "person" : "people"} liked this post.</p>
+      <p>
+        {likes} {likes === 1 ? 'person liked this post.' : 'people liked this post.'}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default ShowLikes;
